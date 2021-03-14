75e5e9054619ef85d07b5ad7124e35144b1fd4f7
moccands <moccandsayeghsara@gmail.com>
2021-03-14 21:33:52 +0100

    populate as row instead of column

--- combination.py
+++ combination.py
@@ -4,12 +4,15 @@ from openpyxl import load_workbook
 
 def seed_comobination(first_col, sec_col) :
     #shit_name = all[first_col]
+    combi = []
     for first_cell in all [first_col][1:] :  
         for sec_cell in all[sec_col][1:] : 
             if (first_cell.value is not None) and (sec_cell.value is not None) :
-                print(first_cell.value + " " + sec_cell.value)
+                #print(first_cell.value + " " + sec_cell.value)
+                combi.append(first_cell.value + " " + sec_cell.value)
             else : 
                 break
+    return combi
 
 def create_comb_col_indices(indices):
     comb_indices = com(indices, 2)
@@ -17,34 +20,38 @@ def create_comb_col_indices(indices):
     print(comb_indices_arr)
     return comb_indices_arr
 
-letter = ["Brand","Topic", "ENtiry"]
+letter = ["Brand","Topic", "Level"]
 x = com(letter,2)
-sheet_names = [i for i in x]
-print(sheet_names)
+col_names = [i for i in x]
+print(col_names)
+
+
 
 #wb = Workbook()
 wb = load_workbook(filename="sample_book.xlsx")
 #for z in y:
-for name in sheet_names :
-    print(name)
-    ws = wb.create_sheet(str(name), 0)
-    ws.title = str(name)
 
 contentSeed = wb.get_sheet_by_name("Seeds")
 all = contentSeed["A:C"]
 
-ws = wb.create_sheet('mexicano', 0)
-wb.save(filename = 'sample_book.xlsx')
+col_indices = create_comb_col_indices([0, 1, 2])
+ind = 0
+for col_name in col_names :
+    print(col_name)
+    sheet_name = col_name[0] + '_' + col_name[1]
+    ws = wb.create_sheet(str(sheet_name), 0)
+    ws.title = str(sheet_name)
+    col_indice = col_indices[ind]
+    combi = seed_comobination(col_indice[0], col_indice[0])
+    ind = ind + 1
+    ws.append(combi)
+
 
 
 col_indices = create_comb_col_indices([0, 1, 2])
 for col_indice in col_indices :
-    seed_comobination(col_indice[0],col_indice[1])
-
-
-#for col in ws.iter_rows(min_row=1, max_row=10, min_col=0, max_col=3, values_only=False) :
-    #print(col)
-
+    combi = seed_comobination(col_indice[0],col_indice[1])
+    print(combi)
 
 
 wb.save(filename = 'sample_book.xlsx')
