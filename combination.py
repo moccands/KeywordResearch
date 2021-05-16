2532feed04bc0aaf1f93ba88a21c143884e0041d
moccands <moccandsayeghsara@gmail.com>
2021-05-16 20:06:58 +0200

    working combi row instead col

--- combination.py
+++ combination.py
@@ -28,8 +28,7 @@ def create_comb_col_indices(indices):
 def create_sheet(sheet_name, first_col_ind, second_col_ind):
     ws = wb.create_sheet(str(sheet_name), 0)
     ws.title = str(sheet_name)
-    col_indice = col_indices[ind]
-    combi = seed_combination(col_indice[0], col_indice[1])
+    combi = seed_combination(first_col_ind, second_col_ind)
     print(type(combi))
 
     for row in combi:
@@ -44,11 +43,7 @@ x = com(title_col, 2)
 col_names = [i for i in x]
 print(col_names)
 
-
-
-#wb = Workbook()
 wb = load_workbook(filename="sample_book.xlsx")
-#for z in y:
 
 contentSeed = wb.get_sheet_by_name("Seeds")
 all = contentSeed["A:C"] # <-modify here
@@ -57,25 +52,11 @@ col_indices = create_comb_col_indices([0, 1, 2]) # <-modify here
 ind = 0
 for col_name in col_names :
     print(col_name)
-    sheet_name = col_name[0] + '_' + col_name[1]
-    ws = wb.create_sheet(str(sheet_name), 0)
-    ws.title = str(sheet_name)
     col_indice = col_indices[ind]
-    combi = seed_combination(col_indice[0], col_indice[1])
-    print(type(combi))
-
-    for row in combi:
-        print(row)
-        row_list = list()
-        row_list.append(row)
-        ws.append(row_list)
+    sheet_name = col_name[0] + '_' + col_name[1]
+    create_sheet(sheet_name, col_indice[0] , col_indice[1] )
+    sheet_name = col_name[1] + '_' + col_name[0]
+    create_sheet(sheet_name, col_indice[1] , col_indice[0] )
     ind = ind + 1
-
-
-#col_indices = create_comb_col_indices([0, 1, 2])
-#for col_indice in col_indices :
-    #combi = seed_comobination(col_indice[0],col_indice[1])
-    #print(combi)
-
 
 wb.save(filename = 'sample_book.xlsx')
